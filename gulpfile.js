20422a68cdbd10555200f62fa577109e10d8140d
Seth Bonnie <sethkbonnie@gmail.com>
2015-01-19 00:49:21 -0600

    Switch from grunt to gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp   = require( 'gulp' );
+var mocha  = require( 'gulp-mocha' );
+var jshint = require( 'gulp-jshint' );
+var uglify = require( 'gulp-uglify' );
+var source = require( 'vinyl-source-stream' );
+var streamify = require( 'gulp-streamify' );
+var browserify = require( 'browserify' );
+
+gulp.task( 'jshint', function() {
+
+  gulp
+    .src( ['gulpfile.js', './src/*.js', './test/*.js'])
+    .pipe( jshint() )
+    .pipe( jshint.reporter( 'default' ) );
+
+});
+
+gulp.task( 'test', ['jshint'], function() {
+
+  gulp
+    .src( 'test/*.js')
+    .pipe( mocha() );
+
+});
+
+// Bundle and compress it all up with browserify + uglify
+gulp.task( 'build', ['test'],  function() {
+
+  browserify( './src/index.js' )
+    .bundle()
+    .pipe( source( 'dispatch.js' ) )
+    .pipe( streamify(uglify()) )
+    .pipe( gulp.dest( './dist' ) );
+
+});
+
+gulp.task( 'default', ['build'] );
