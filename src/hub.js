9f2fae985440f25408bbccc5e79ec97932cad72f
Seth Bonnie <sethkbonnie@gmail.com>
2015-04-07 20:07:45 -0500

    Use clearer names

--- src/hub.js
+++ src/hub.js
@@ -18,6 +18,115 @@ global.PubHub = module.exports = function Hub() {
   var _subscribers = Object.create( null );
 
 
+  /** Hub#dispatch( message, payload )
+    * Sends the payload to all subscribers of the signal
+    * 
+    * @param {string} message - The message to send to each subscriber. This
+    *   string should contain any wildcard characters. This akin to how an 
+    *   office building might receive a lot mail for different recipients, yet 
+    *   in order for the mail to actually get to the recipient, the sender 
+    *   has to be explicit about who it is for.
+    * @param {any} payload - The data to send to each subscriber.
+    *
+    * @returns {undefined}
+    */
+  hub.dispatch = function( message, payload ) {
+
+    var matchingSubs = [];
+    var subscriberMessages = Object.keys( _subscribers );
+
+    var matchingPatterns;
+    var key;
+    var i;
+
+    // Don't allow wildcards in emissions.
+    if ( message.indexOf( '*' ) != -1 ) {
+      throw Error( 'Wildcard patterns are not allowed in emissions' );
+    }
+
+    matchingPatterns = subscriberMessages.filter( function( pattern ) {
+      return globToRegex( pattern ).test( message );
+    });
+
+    // Gather all the matching subscribers into one array
+    for ( i = 0; i < matchingPatterns.length; i++ ) {
+      key = matchingPatterns[i];
+      matchingSubs = matchingSubs.concat( _subscribers[key] );
+    }
+
+    // Filter out duplicates
+    matchingSubs = unique( matchingSubs );
+
+    // Send the message to each subscriber
+    for ( i = 0; i < matchingSubs.length; i++ ) {
+      (function(fn, msg, pay ) {
+        setImmediate( function() {
+          fn( msg, pay );
+        });
+      })( matchingSubs[i], message, payload )
+    }
+
+  };
+
+
+  /** Hub#sub( message(s), subscriber )
+    * Registers the `sub` to receive all signals of type `message`.
+    * @param {String|Array} messages - A message pattern or array of messages that the 
+    *   `subscriber` wishes to receive. Each message can have any number of wildcards
+    *   which will match zero or more word-like characters.
+    * @param {Function} subscriber - The function that will be the recipient of each
+    *   subscribed message and its associated payload.
+    * @returns {undefined}
+    */
+  hub.sub = function( messages, subscriber ) {
+    var message;
+    var matchingSubs;
+    var len;
+    var i;
+
+    if ( typeof messages === 'string' ) {
+      messages = [ messages ];
+    }
+
+    if ( !( messages instanceof Array ) ) {
+      throw new Error( 
+        '`message` must be either a message string or an array of messages' );
+    }
+
+    if ( typeof subscriber !== 'function' ) {
+      throw new Error( 'Missing or invalid argument: `subscriber`' );
+    }
+
+    // Loop through each message and register the sub to the _subscribers object.
+    for ( i = 0, len = messages.length; i < len; i++ ) {
+
+      message = messages[i];
+
+      if ( typeof message !== 'string' ) {
+        throw new Error( 
+          '`message` must be a string' );
+      }
+
+      matchingSubs = _subscribers[ message ];
+
+      /** If there is a mapping from the message to a matching subscribers 
+        * list, add our subscriber if it's not already part of the list.
+        * Otherwise, there is no mapping, create a new list.
+        */
+      if ( matchingSubs ) {
+        if ( matchingSubs.indexOf( subscriber ) < 0 ) {
+          matchingSubs.push( subscriber );
+        } 
+      }
+      else {
+        _subscribers[ message ] = [ subscriber ];
+      }
+    }
+
+    return subscriber;
+  };
+
+
   /** Hub#unsub( messages, subscriber )
     * Stops routing messages of type <`module`:`signal`> to the subscriber.
     * 
@@ -87,113 +196,6 @@ global.PubHub = module.exports = function Hub() {
         delete _subscribers[ matchingPatterns[i] ];
       }
     }
-  };
-
-  /** Hub#dispatch( message, payload )
-    * Sends the payload to all subscribers of the signal
-    * 
-    * @param {string} message - The message to send to each subscriber. This
-    *   string should contain any wildcard characters. This akin to how an 
-    *   office building might receive a lot mail for different recipients, yet 
-    *   in order for the mail to actually get to the recipient, the sender 
-    *   has to be explicit about who it is for.
-    * @param {any} payload - The data to send to each subscriber.
-    *
-    * @returns {undefined}
-    */
-  hub.dispatch = function( message, payload ) {
-
-    var matchingSubs = [];
-    var subscriberMessages = Object.keys( _subscribers );
-
-    var matchingPatterns;
-    var key;
-    var i;
-
-    // Don't allow wildcards in emissions.
-    if ( message.indexOf( '*' ) != -1 ) {
-      throw Error( 'Wildcard patterns are not allowed in emissions' );
-    }
-
-    matchingPatterns = subscriberMessages.filter( function( pattern ) {
-      return globToRegex( pattern ).test( message );
-    });
-
-    // Gather all the matching subscribers into one array
-    for ( i = 0; i < matchingPatterns.length; i++ ) {
-      key = matchingPatterns[i];
-      matchingSubs = matchingSubs.concat( _subscribers[key] );
-    }
-
-    // Filter out duplicates
-    matchingSubs = unique( matchingSubs );
-
-    // Send the message to each subscriber
-    for ( i = 0; i < matchingSubs.length; i++ ) {
-      (function(fn, msg, pay ) {
-        setImmediate( function() {
-          fn( msg, pay );
-        });
-      })( matchingSubs[i], message, payload )
-    }
-
-  };
-
-  /** Hub#sub( message(s), subscriber )
-    * Registers the `sub` to receive all signals of type `message`.
-    * @param {String|Array} messages - A message pattern or array of messages that the 
-    *   `subscriber` wishes to receive. Each message can have any number of wildcards
-    *   which will match zero or more word-like characters.
-    * @param {Function} subscriber - The function that will be the recipient of each
-    *   subscribed message and its associated payload.
-    * @returns {undefined}
-    */
-  hub.sub = function( messages, subscriber ) {
-    var message;
-    var subs;
-    var len;
-    var i;
-
-    if ( typeof messages === 'string' ) {
-      messages = [ messages ];
-    }
-
-    if ( !( messages instanceof Array ) ) {
-      throw new Error( 
-        '`message` must be either a message string or an array of messages' );
-    }
-
-    if ( typeof subscriber !== 'function' ) {
-      throw new Error( 'Missing or invalid argument: `subscriber`' );
-    }
-
-    // Loop through each message and register the sub to the _subscribers object.
-    for ( i = 0, len = messages.length; i < len; i++ ) {
-
-      message = messages[i];
-
-      if ( typeof message !== 'string' ) {
-        throw new Error( 
-          '`message` must be a string' );
-      }
-
-      subs = _subscribers[ message ];
-
-      /** Check if there is a mapping from the message to a subscribers 
-        * list; if one exists, add our subscriber if it's not already part of the list.
-        * If there is no mapping then create a new list.
-        */
-      if ( subs ) {
-        if ( subs.indexOf( subscriber ) < 0 ) {
-          subs.push( subscriber );
-        } 
-      }
-      else {
-        _subscribers[ message ] = [ subscriber ];
-      }
-    }
-
-    return subscriber;
   };
 
   return hub;
